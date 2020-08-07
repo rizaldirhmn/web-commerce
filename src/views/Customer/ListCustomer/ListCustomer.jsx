ee1d6bdb0d09cd85f3e8758375e69d0bd47aef39
Rizaldi Rahman <rizaldirahman0@gmail.com>
2020-08-07 15:26:46 +0700

    change data on list customer

--- src/views/Customer/ListCustomer/ListCustomer.jsx
+++ src/views/Customer/ListCustomer/ListCustomer.jsx
@@ -10,52 +10,22 @@ import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 
 const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
+  { id: 'no', 'label' : 'No',minWidth: 100},
+  { id: 'nama', label: 'Nama', minWidth: 170 },
+  { id: 'no_id', label: 'No ID', minWidth: 100 },
+  { id: 'tipe', label: 'Tipe', minWidth: 100 },
+  { id: 'no_hp', label: 'Nomor HP', minWidth: 100 },
+  { id: 'status', label: 'Status', minWidth: 100 },
+  { id: 'action', label: 'Action', minWidth: 170 },
+  
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
+function createData(no, nama, no_id, tipe, no_hp, status, action) {
+  return { no, nama, no_id, tipe, no_hp, status, action };
 }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+  createData('1', 'Rizaldi Rahman', 1324171354, 'Lorem Ipsum','08123123123','Aktif',''),
 ];
 
 const useStyles = makeStyles({
