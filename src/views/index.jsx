b114a580ff417fb8e4b2e66dcd52fb12435df124
Rizaldi Rahman <rizaldirahman0@gmail.com>
2020-11-11 11:17:19 +0700

    create task

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -2,3 +2,4 @@ export { default as Dashboard } from './Dashboard';
 export { default as Profile } from './Profile';
 export { default as Home } from './Home'
 export { default as Customer } from './Customer'
+export { default as Task } from './Task'
